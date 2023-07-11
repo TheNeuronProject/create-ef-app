a0a55221870df199812d851ee0e8de4143ce54d4
Yukino Song <yukino@yuki.network>
2023-07-11 23:16:06 +0800

    Update template
    Signed-off-by: Yukino Song <yukino@yuki.network>

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 
-const { resolve } = require('path');
-const { create } = require('create-initializer');
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { create } from 'create-initializer'
 
-const templateRoot = resolve(__dirname, '..', 'templates');
+const templateRoot = resolve(dirname(fileURLToPath(import.meta.url)), '..', 'templates')
 
-const caveat = `
-To start a dev server, run "npm start" in your project directory.
+const startCommands = {
+  npm: 'npm start',
+  pnpm: 'pnpm start',
+  yarn: 'yarn start'
+}
+
+const caveat = ({projectDir, answers}) => `
+To start the dev server, simply run "${startCommands[answers.packageManager]}" in your project directory:
+
+  cd ${projectDir}
+  ${startCommands[answers.packageManager]}
+
 See "README.md" for more details.
-Happy playing around with ef.js!
-`;
-
-// See https://github.com/uetchy/create-create-app/blob/master/README.md for the all options.
+Happy creating great projects with ef.js!
+`
 
 create('create-ef-app', {
   templateRoot,
   templatePrefix: 'ef-starter-',
   promptForTemplate: true,
   defaultTemplate: 'rollup',
-  caveat,
-});
+  defaultPackageManager: 'pnpm',
+  caveat
+})
